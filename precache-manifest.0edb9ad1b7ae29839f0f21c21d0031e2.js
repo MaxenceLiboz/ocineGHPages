29ecce518e3f6fb53dcaecc4e14c2e94121e67ce
Maxence <maxoulib@gmail.com>
2020-08-11 19:07:14 +0200

    Updates

---
+++ precache-manifest.0edb9ad1b7ae29839f0f21c21d0031e2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1a05b8794f7e52749b752ad046766af",
+    "url": "/ocineGHPages/index.html"
+  },
+  {
+    "revision": "971e2add92781b6bdbf4",
+    "url": "/ocineGHPages/static/css/2.1295bded.chunk.css"
+  },
+  {
+    "revision": "c008edcd595488455689",
+    "url": "/ocineGHPages/static/css/main.758a008b.chunk.css"
+  },
+  {
+    "revision": "971e2add92781b6bdbf4",
+    "url": "/ocineGHPages/static/js/2.8b9cfd6b.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/ocineGHPages/static/js/2.8b9cfd6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c008edcd595488455689",
+    "url": "/ocineGHPages/static/js/main.c2feae55.chunk.js"
+  },
+  {
+    "revision": "d1372fba24e0d34475f7",
+    "url": "/ocineGHPages/static/js/runtime-main.796d4aaa.js"
+  },
+  {
+    "revision": "43aeec46f86dc9e0614580a05d0737be",
+    "url": "/ocineGHPages/static/media/blueOcean.43aeec46.jpg"
+  },
+  {
+    "revision": "a5df8fa2fd8ba742422e5690f06bdef5",
+    "url": "/ocineGHPages/static/media/boxerBlue.a5df8fa2.jpg"
+  },
+  {
+    "revision": "7be9478e8c12eaec9c1c228456a16316",
+    "url": "/ocineGHPages/static/media/capFR.7be9478e.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/ocineGHPages/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/ocineGHPages/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/ocineGHPages/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/ocineGHPages/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "7c0ec23a3ec594f0d54edf22290ffcb2",
+    "url": "/ocineGHPages/static/media/suitBlue.7c0ec23a.jpg"
+  },
+  {
+    "revision": "ad257f182892bdd22f1b4b91ff2ee7e5",
+    "url": "/ocineGHPages/static/media/team.ad257f18.jpg"
+  }
+]);
